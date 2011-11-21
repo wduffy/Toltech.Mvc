4109f477f15d34167880233b6e6aaca37c71e57f
William Duffy <info@wduffy.co.uk>
2011-11-21 23:24:31 +0000

    Added an extension method to easily select model binding values

---
+++ Extensions/System.Web.Mvc/ModelBindingContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace System.Web.Mvc
+{
+    public static class ModelBindingContextExtensions
+    {
+
+        public static T GetValue<T>(this ModelBindingContext bindingContext, string key)
+        {
+            if (String.IsNullOrWhiteSpace(key))
+                return default(T);
+
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName + "." + key);
+
+            if (value == null && bindingContext.FallbackToEmptyPrefix == true) //Didn't work? Try without the prefix if needed...
+                value = bindingContext.ValueProvider.GetValue(key);
+
+            if (value == null)
+                return default(T);
+
+            return (T)value.ConvertTo(typeof(T));
+        }
+
+    }
+}
