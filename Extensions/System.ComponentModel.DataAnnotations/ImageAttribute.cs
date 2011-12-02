95773d034e516de4d7709ff88d78111b7f178614
William Duffy <info@wduffy.co.uk>
2011-12-02 16:50:28 +0000

    Added String With Attributes

---
+++ Extensions/System.ComponentModel.DataAnnotations/ImageAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace System.ComponentModel.DataAnnotations
+{
+    public class ImageAttribute : EndsWithAttribute
+    {
+        /// <summary>
+        /// Checks the current string to see if it ends with the value provided.
+        /// </summary>
+        /// <param name="value">The value to check for as a regular expression</param>
+        public ImageAttribute() : base("jpeg", "jpg", "gif", "png")
+        {
+        }
+
+    }
+}
---
+++ Extensions/System.ComponentModel.DataAnnotations/WithAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace System.ComponentModel.DataAnnotations
+{
+    public abstract class WithAttribute : ValidationAttribute
+    {
+        private Part _part;
+        public string[] Values
+        {
+            get;
+            private set;
+        }
+
+        public WithAttribute(Part part, params string[] values)
+            : base(string.Format("The {{0}} field must {0} with \"{1}\".", part.ToString().ToLower(), string.Join("\" or \"", values)))
+        {
+            _part = part;
+            Values = values;         
+        }
+
+        public override bool IsValid(object obj)
+        {
+            string value = (string)obj;
+
+            if (string.IsNullOrEmpty(value))
+                return true;
+
+            return
+                _part == Part.Start ?
+                Values.Any(x => value.StartsWith(x)) :
+                Values.Any(x => value.EndsWith(x));
+
+        }
+
+        public enum Part
+        {
+            Start,
+            End
+        }
+
+    }
+
+    public class StartsWithAttribute : WithAttribute
+    {
+
+        /// <summary>
+        /// Checks the current string to see if it starts with the value provided.
+        /// </summary>
+        /// <param name="values">The values to check the current string for</param>
+        public StartsWithAttribute(params string[] values) : base(Part.Start, values) { }
+
+    }
+
+    public class EndsWithAttribute : WithAttribute
+    {
+
+        /// <summary>
+        /// Checks the current string to see if it ends with the value provided.
+        /// </summary>
+        /// <param name="values">The values to check the current string for</param>
+        public EndsWithAttribute(params string[] values) : base(Part.End, values) { }
+
+    }
+
+}
